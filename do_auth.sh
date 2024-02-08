c9daaa2e7a29806d3adb36b00f50790e795de88c
John Singleton <john@looseend.org>
2024-02-08 16:16:52 +0000

    comments

--- do_auth.sh
+++ do_auth.sh
@@ -1,8 +1,13 @@
 #!/bin/zsh
+
+# Use a SAMLResponse to get an auth code and then use that to get a token, using PKCE
+
 SSO_URL=${SSO_URL:-https://sso.8x8pilot.com/}
 client_id=${client_id:-vom_8_android}
 client_id_64=$(echo -n ${client_id}\: | base64)
 subject=${1-smart.test}
+
+# Create the verifier and challenge for PKCE
 
 code_verifier=$(LC_CTYPE=C && LANG=C && cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 50 | head -n 1)
 code_challenge=$(echo -n $code_verifier | shasum -a 256 | cut -d " " -f 1 | xxd -r -p | base64 | tr / _ | tr + - | tr -d =)
@@ -14,18 +19,18 @@ echo "Client: $client_id"
 echo "Client64: ${client_id_64}"
 echo "Subject=${subject}"
 
+# Use the Java app to create the signed SAMLResponse
 
 SAMLResponse=$(java -jar target/saml-generator-1.0.jar -subject ${subject}  -issuer com.smart.alpha -privateKey smart.pkcs8 -publicKey smart.crt | base64)
+
+# Authenticate using the SAMLResponse
+# Note the use of a cookie jar, this maintains the authenticated session so we can get an auth code in the next step
 
 curl --cookie cookie.txt --cookie-jar cookie.txt -s -D - --request POST --url ${SSO_URL}saml2 --header 'Content-Type: application/x-www-form-urlencoded' \
   --data-urlencode "SAMLResponse=${SAMLResponse}" \
   --data-urlencode "RelayState=target=app://com.android" -o /dev/null
 
-# https://sso.localtest.com/v2/oauth/authorize?response_type=code&client_id=cloud8-pkce&
-# redirect_uri=https%3A%2F%2Fgk.localtest.com%2Flogin
-# &state=123
-# &code_challenge=FtSFepBMr5424knXdSeqwyj5JJc6IJskskYMfh1GIG4
-
+# Get the auth code using the cookie jar to maintain the session
 echo "get code"
 redirect=$(curl --cookie cookie.txt --cookie-jar cookie.txt -s -S  --request POST --url ${SSO_URL}v2/oauth/authorize \
   --header 'Content-Type: application/x-www-form-urlencoded' \
@@ -40,19 +45,12 @@ redirect=$(curl --cookie cookie.txt --cookie-jar cookie.txt -s -S  --request POS
   --data-urlencode "code_challenge_method=S256" \
   -o /dev/null)
 
-# | grep -Fi location | sed -r 's/.*"(.*)".*/\1/'
-echo "Location is ${redirect}"
-
+# Extract the auth code from the redirect URL
 auth_code=$(echo ${redirect} | sed -e 's/.*?code=\([^&]*\).*$/\1/g')
 
 echo "get token for code: ${auth_code}"
 
-# | grant_type=authorization_code
-# &code=c7b608.gWqNg13bgufxJBKt
-# &redirect_uri=https%3A%2F%2Fgk.localtest.com%2Flogi
-# n&state=123
-# &code_verifier=FtSFepBMr5424knXdSeqwyj5JJc6IJskskYMfh1GIG4
-
+# Exchange the auth code for a token
 
 curl --cookie cookie.txt --cookie-jar cookie.txt -sS --request POST --url ${SSO_URL}v2/oauth/token \
   --header 'Content-Type: application/x-www-form-urlencoded' \
